99570c3417316b68bd9d287192981b061c19a341
smaheshacharya <mahesh01acharya@gmail.com>
2019-04-08 22:18:38 +0545

    add 'romove_stop_word' function

--- Analysis.py
+++ Analysis.py
@@ -10,28 +10,47 @@ second_row  = df.ix[1:,1]
 # print(second_row)
 data_with_split = []
 each_docs = {}
+stop_words_split_final=[]
 def split_doc():
 	for data in first_row:
 		each_docs = data.split()
 		data_with_split.append(each_docs)
 	return data_with_split # it returns arr of each docs with spleted words
-
-
 word_lists = {}
-word_lists = split_doc() # your original lists
+word_lists = split_doc() 
 length_of_docs = len(word_lists)
-print(length_of_docs)
 my_set = set.union(*map(set,word_lists))# seperate each individual words from data to make matrix
+print(len(my_set))
+#remove stop words 
+##########################################
+f = open('stopwords.txt', 'r')
+def split_stop_words():
+	for line in f:
+		stop_words_split = line.split()
+		stop_words_split_final.append(stop_words_split)
+	return(stop_words_split_final)
+# functopn end
+stop_word_collection = {}
+stop_word_collection = split_stop_words()
+stop_word_dict = set.union(*map(set,stop_word_collection))
+my_set = my_set-stop_word_dict
+# print(stop_word_dict)
+print("afetr removiing stop word")
+# print(len(my_set))
+########################################################
 word_dict  = {}
 for i in range(1,len(my_set)):
 	word_dict = dict.fromkeys(my_set,0)
-
+print(word_dict)
 
 
 ### count each words from each docs
 for count_word_value in word_lists:
 	for word in count_word_value:
-	    word_dict[word] += 1
+		if word in word_dict:
+			word_dict[word] += 1
+print(word_dict)
+
 def computeTf(worddict,docs_list):
 	tfDict = {}
 	doc_word_count = len(docs_list)
